142c3ad0aeb5ed7d13fe428a813668262c653ce1
Shubham yadav <shubhamyadav1662000@gmail.com>
2026-04-10 20:27:02 +0530

    dynamicaly create static page after fetch data from somewhere else

--- 04ssg-ssr-csr/app/blogs/[blog]/page.jsx
+++ 04ssg-ssr-csr/app/blogs/[blog]/page.jsx
@@ -1,13 +1,25 @@
 
 
-export function generateStaticParams(){
-return [
-  {blog:"1"},
-  {blog:"2"},
-  {blog:"3"},
-  {blog:"4"},
-  {blog:"5"},
-]
+export async function generateStaticParams(){
+
+  const response=await fetch('https://jsonplaceholder.typicode.com/todos')
+  const result=await response.json()
+  console.log(result);
+  
+return result.map((item)=>({ blog :`${item.id}`})
+
+)
+
+
+
+//   return [
+//   {blog:"1"},
+//   {blog:"2"},
+//   {blog:"3"},
+//   {blog:"4"},
+//   {blog:"5"},
+// ]
+
 }
 
 
