41ea78d9bda61718003a4275afea1525613b46c0
Shubham yadav <shubhamyadav1662000@gmail.com>
2026-04-14 12:18:06 +0530

    manualy created hydration error

---
+++ 05hydration-in-next/app/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+export default function Home() {
+if(typeof window==="undefined"){
+  return <div>This is Server side rendering</div>
+}
+
+
+  return (
+    <>
+    <h1>This is Server page which server render that is called Server Side Rendering</h1>
+    </>
+  );
+}
