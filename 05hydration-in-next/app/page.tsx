6982ba2c6ca7b66693ff8ea7aa0e38feab83b47f
Shubham yadav <shubhamyadav1662000@gmail.com>
2026-04-14 12:26:29 +0530

    remove hydreation error

--- 05hydration-in-next/app/page.tsx
+++ 05hydration-in-next/app/page.tsx
@@ -1,11 +1,11 @@
 'use client'
 
 export default function Home() {
-if(typeof window!=="undefined"){
+// if(typeof window!=="undefined"){
 
   
-  return <div>This is Server side rendering</div>
-}
+//   return <div>This is Server side rendering</div>
+// }
 
 
   return (
