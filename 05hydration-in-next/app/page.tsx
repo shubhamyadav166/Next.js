5bec79080ec2ebebc6a6b08e913fe2b90360fde8
Shubham yadav <shubhamyadav1662000@gmail.com>
2026-04-14 12:36:08 +0530

    here application is made fool by us we are returning same div conditionaly different for server and client

--- 05hydration-in-next/app/page.tsx
+++ 05hydration-in-next/app/page.tsx
@@ -1,16 +1,16 @@
 'use client'
 
 export default function Home() {
-// if(typeof window!=="undefined"){
+if(typeof window!=="undefined"){
 
   
-//   return <div>This is Server side rendering</div>
-// }
+  return <div>This is Server side rendering</div>
+}
 
 
   return (
     <>
-    <h1>This is Server page which server render that is called Server Side Rendering</h1>
+    <div>This is Server side rendering</div>
     </>
   );
 }
