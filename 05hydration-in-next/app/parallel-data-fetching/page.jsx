0072b60aba574dfaafcecd656e2bc51cc93a3614
Shubham yadav <shubhamyadav1662000@gmail.com>
2026-04-16 17:00:33 +0530

    code customize remove one Promise.all from the code

--- 05hydration-in-next/app/parallel-data-fetching/page.jsx
+++ 05hydration-in-next/app/parallel-data-fetching/page.jsx
@@ -1,37 +1,41 @@
 
+async function fetchData(url){
+ const response= await fetch(url)
+ return await response.json()
+
+}
+
 
 export default async function page() {
-   const data=await Promise.all([
-    fetch('https://procodrr.vercel.app/?sleep=2000'),
-    fetch("https://jsonplaceholder.typicode.com/users?_limit=6"),
-    fetch("https://jsonplaceholder.typicode.com/users?_limit=10")
+  //  const data=await Promise.all([
+  //   fetch('https://procodrr.vercel.app/?sleep=2000'),
+  //   fetch("https://jsonplaceholder.typicode.com/users?_limit=6"),
+  //   fetch("https://jsonplaceholder.typicode.com/users?_limit=10")
+  //  ])   
+  //  const fetchedData=await Promise.all(data.map((item)=>item.json()))
+  //  const [data1,data2,data3]=fetchedData
+  //  console.log(fetchedData);
+   
+   
+  const [res1,res2,res3]= await Promise.all([
+    fetchData('https://procodrr.vercel.app/?sleep=2000'),
+    fetchData('https://jsonplaceholder.typicode.com/users?_limit=6'),
+    fetchData('https://jsonplaceholder.typicode.com/users?_limit=10')
    ])
 
-  //  console.log(data);
-   
-   const fetchedData=await Promise.all(data.map((item)=>item.json()))
-   const [data1,data2,data3]=fetchedData
-   console.log(fetchedData);
-   
-  //  console.log(data1);
-  //  console.log(data2);
-  //  console.log(data3);
-   
-   
-const url=['https://procodrr.vercel.app/?sleep=2000','https://jsonplaceholder.typicode.com/users?_limit=6','https://jsonplaceholder.typicode.com/users?_limit=10']
 
 
   return (
     <div>
-      <h1>{data1.message}</h1>
+      <h1>{res1.message}</h1>
       <div>
-        {data2.map((item)=>(
+        {res2.map((item)=>(
           
         <div key={item.id}>{item.name}</div>
         ))}
       </div>
       <div>
-        {data3.map((item)=>(
+        {res3.map((item)=>(
           
         <div key={item.id}>{item.name}</div>
         ))}
