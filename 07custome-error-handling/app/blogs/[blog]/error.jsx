2a1343eef1c88d0652b6bd6a9127e6006a912fc4
Shubham yadav <shubhamyadav1662000@gmail.com>
2026-04-19 11:04:09 +0530

    error handled gracefully using startTransition and router.refresh() and reset()

--- 07custome-error-handling/app/blogs/[blog]/error.jsx
+++ 07custome-error-handling/app/blogs/[blog]/error.jsx
@@ -1,12 +1,23 @@
 'use client'
+import {useRouter} from 'next/navigation'
+import { startTransition } from "react"
 
 export default function error({ error, reset }) {
-
+const router=useRouter()
     
   return (
     <div>
         <h1>Somthing Went Wronge: {error.message}</h1>
-        <button onClick={()=>reset()} className="text-white bg-red-500 rounded">Try Again</button>
+       
+        <button onClick={()=>{
+startTransition(()=>{
+    router.refresh()
+    reset()
+})
+           
+
+        }
+            } className="text-white bg-red-500 rounded">Try Again</button>
         </div>
   )
 }
--- 07custome-error-handling/app/blogs/[blog]/page.jsx
+++ 07custome-error-handling/app/blogs/[blog]/page.jsx
@@ -2,13 +2,21 @@
 
 
 export default async function page({params}) {
-console.log(await params);
-const {blog}=await params
-if(Number(blog%2===0)){
-    throw new Error("Even Page Can't be seen")
+// console.log(await params);
+// const {blog}=await params
+// if(Number(blog%2===0)){
+//     throw new Error("Even Page Can't be seen")
+// }
+// second approach to handle error 
+
+const value=Math.random()
+console.log(value);
+
+if(value>0.5){
+    throw new Error("This is the error page ")
 }
    
   return (
-    <div>Indivisual Blog Page: {blog}</div>
+    <div>Indivisual Blog Page: </div>
   )
 }
