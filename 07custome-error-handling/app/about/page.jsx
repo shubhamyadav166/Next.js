9f02319027bffb2d66348dd70eab1ff7e6f89ce3
Shubham yadav <shubhamyadav1662000@gmail.com>
2026-04-20 13:34:32 +0530

    client side error is in about page button onclick and server-side at rendering error at force-dynamic page

---
+++ 07custome-error-handling/app/about/page.jsx
@@ -0,0 +1,19 @@
+'use client'
+
+export default function page() {
+
+  return (
+    <div>
+        <h1>This is About Page</h1>
+        <button onClick={()=>{
+            console.log(object);
+            
+        }}>
+            Click Me
+            </button> 
+
+
+    </div>
+  )
+}
+
--- 07custome-error-handling/app/server-error/page.jsx
+++ 07custome-error-handling/app/server-error/page.jsx
@@ -1,16 +1,13 @@
-'use client'
-
-import { useEffect } from "react";
-
+export const dynamic = 'force-dynamic'
 export default function page() {
-useEffect(()=>{
+
     const value=Math.random()
     console.log(value);
     
     if(value>.5){
         throw  Error("Error Occurred")
     }
-},[])
+
   return (
     <div>Client Side Error Page</div>
   )
