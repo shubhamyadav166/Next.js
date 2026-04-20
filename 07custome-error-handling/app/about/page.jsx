3eea68a2bf11d05011f36d039f0ad2f0d0fab9c7
Shubham yadav <shubhamyadav1662000@gmail.com>
2026-04-20 13:42:28 +0530

    global-error.js file handle each type of error including client side rendering error which error is in about page and handled by global file

--- 07custome-error-handling/app/about/page.jsx
+++ 07custome-error-handling/app/about/page.jsx
@@ -1,13 +1,22 @@
 'use client'
 
+import { useState } from "react";
+
 export default function page() {
+  const [fruit,setFruit]=useState(["Mango,Banana"])
 
   return (
     <div>
         <h1>This is About Page</h1>
+      
+      <div>
+        {fruit.map((item)=>(
+          <p key={item}>{item}</p>
+        ))}
+      </div>
         <button onClick={()=>{
-            console.log(object);
-            
+          setFruit(null)
+          
         }}>
             Click Me
             </button> 
