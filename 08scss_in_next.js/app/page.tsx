5ec5cea1a986ec5612e526d8a2e502aab3281383
Shubham yadav <shubhamyadav1662000@gmail.com>
2026-04-22 20:37:17 +0530

    finaly understand use image of other cdn

--- 08scss_in_next.js/app/page.tsx
+++ 08scss_in_next.js/app/page.tsx
@@ -7,13 +7,21 @@ export default function Home() {
       <div className='w-[700px] h-[700px]'>
         <h1>Home Image</h1>
         {/* using simple image tag without optimization */}
-        <img className='w-[300px] h-[200px]' src="/nature_img.jpg" alt="" />
+        <img className='w-[700px] ' src="/nature_img.jpg" alt="" />
 
         <Image src="/nature_img.jpg" 
-        width={400} 
-        height={300} 
+        width={700} 
+        height={475} 
         alt="Home Image nature view"
         quality={1} 
+        // unoptimized
+        />
+        <Image src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e" 
+        width={700} 
+        height={475} 
+        alt="camera Image nature view"
+        quality={75} 
+        // unoptimized
         />
       </div>
     </>
